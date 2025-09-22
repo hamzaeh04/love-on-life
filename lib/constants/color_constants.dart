6ea489e1e4bd74fc4009c75a84ea88bf438c70f9
canvas.training <canvas.training@int.sybrid.com>
2025-09-23 03:25:44 +0500

    Login Page

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -6,6 +6,8 @@ const whiteColor = Colors.white;
 const blackColor = Colors.black;
 const backgroundColor = Color(0xFFF6FDFF);
 const buttonPinkColor = Color(0xFFA61780);
+const loginGreyColor = Color(0xFF666666);
+const textfieldBorderColor = Color(0xFFC3C7EC);
 
 // const Color(0xFF604D7A);
 
