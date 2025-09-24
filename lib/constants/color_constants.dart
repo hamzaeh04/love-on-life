c61117962aab81b35081d9ae7bccf7a8910af722
hamzaeh04 <hamza.ehsan@nextappinc.com>
2025-09-25 01:50:32 +0500

    Chnages in Colors

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -12,8 +12,9 @@ const resetBlueColor = Color(0xFF2C608F);
 const navIconBlueColor = Color(0xFF34A0C2);
 const navIconGreyColor = Color(0xFFB3B9BA);
 const containerBgBlueColor = Color(0xFFD9F3FB);
-const publicButtonColor = Color(0xFFDEE2FF);
 const selectedTabColor = Color(0xFFD9F3FB);
+const publicButtonColor = Color(0xFFDEE2FF);
+const joinButtonColor = Color(0xFFD9F3FB);
 
 // const Color(0xFF604D7A);
 
