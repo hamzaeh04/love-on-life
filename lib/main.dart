16adc0b3dfd3a3173a1ece071094332f611edf7c
muhammad-ahmed-73 <muhammad.ahmed@canvasdigital.org>
2026-05-04 21:32:47 +0500

    Commit before pull

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,6 @@ void main() async{
 Future<void> firebaseMessageBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   print(message.notification!.title.toString());
-
 }
 
 class MyApp extends StatelessWidget {
